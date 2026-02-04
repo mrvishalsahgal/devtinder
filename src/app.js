c30600ff103bf1497ce892795ae901c57391d0d1
Vishal Sahgal <mrvishalsahgal@gmail.com>
2026-02-04 23:01:11 +0530

    feat: exploring get post put delete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.use("/vishal", (req, res) => {
-  res.send("Hello World!");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Vishal", lastName: "Sahgal", age: 22 });
+});
+
+app.post("/user", (res, req) => {
+  const data = console.log("cehcking curosity");
+  req.send("User Data Changed" + data);
+});
+
+app.put("/user", (res, req) => {
+  req.send("User Data Updated");
+});
+
+app.delete("/user", (res, req) => {
+  req.send("User Data Deleted");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Testing the route");
 });
 
 app.listen(3000, () => {
