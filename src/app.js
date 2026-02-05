201252af9c6e7becb48723871fe9ac43a9dbbc83
Vishal Sahgal <mrvishalsahgal@gmail.com>
2026-02-05 13:37:58 +0530

    feat : Multiple Route handler

--- src/app.js
+++ src/app.js
@@ -1,26 +1,42 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:id/:name/:age", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Vishal", lastName: "Sahgal", age: 22 });
-});
+// app.get("/user/:id/:name/:age", (req, res) => {
+//   console.log(req.params);
+//   res.send({ firstName: "Vishal", lastName: "Sahgal", age: 22 });
+// });
 
-app.post("/user", (res, req) => {
-  req.send("User Data Changed");
-});
+// app.post("/user", (res, req) => {
+//   req.send("User Data Changed");
+// });
 
-app.put("/user", (res, req) => {
-  req.send("User Data Updated");
-});
+// app.put("/user", (res, req) => {
+//   req.send("User Data Updated");
+// });
 
-app.delete("/user", (res, req) => {
-  req.send("User Data Deleted");
-});
+// app.delete("/user", (res, req) => {
+//   req.send("User Data Deleted");
+// });
 
-app.get("/test", (req, res) => {
-  res.send("Testing the route");
-});
+// app.get("/test", (req, res) => {
+//   res.send("Testing the route");
+// });
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Route handler 1");
+    next();
+  },
+  (req, res) => {
+    console.log("Route handler 2");
+    res.send("User Data 2");
+  },
+  (req, res) => {
+    console.log("Route handler 3");
+    res.send("User Data 3");
+  },
+);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
