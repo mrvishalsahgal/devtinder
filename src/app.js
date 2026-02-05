bf28bcdf5421b9cc3b18a555c07fcfd499c49fea
Vishal Sahgal <mrvishalsahgal@gmail.com>
2026-02-05 16:31:46 +0530

    feat : middleware and error handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { adminAuth, userAuth } = require("./middleware/auth");
 
 // app.get("/user/:id/:name/:age", (req, res) => {
 //   console.log(req.params);
@@ -18,25 +19,55 @@ const app = express();
 //   req.send("User Data Deleted");
 // });
 
-// app.get("/test", (req, res) => {
-//   res.send("Testing the route");
-// });
-
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Route handler 1");
+app.get("/test", (req, res, next) => {
+  try {
+    console.log("Test Route");
     next();
-  },
-  (req, res) => {
-    console.log("Route handler 2");
-    res.send("User Data 2");
-  },
-  (req, res) => {
-    console.log("Route handler 3");
-    res.send("User Data 3");
-  },
-);
+    throw new Error("Something went wrong");
+  } catch (error) {
+    next(error);
+    res.send("Testing the route");
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong global");
+  }
+});
+
+app.use("/admin", adminAuth);
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("Admin Data");
+});
+app.get("/admin/getSpecificData", (req, res) => {
+  res.send("Admin Specific Data");
+});
+
+app.get("/user/login", (req, res) => {
+  res.send("User Login");
+});
+
+app.get("/user/getAllData", userAuth, (req, res) => {
+  res.send("User Data");
+});
+
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Route handler 1");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("Route handler 2");
+//     res.send("User Data 2");
+//   },
+//   (req, res) => {
+//     console.log("Route handler 3");
+//     res.send("User Data 3");
+//   },
+// );
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
---
+++ src/middleware/auth.js
@@ -0,0 +1,21 @@
+const adminAuth = (req, res, next) => {
+  const token = "xydz";
+  if (token === "xyz") {
+    console.log("Admin Auth sucessful");
+    next();
+  } else {
+    res.status(401).send("Invalid Token");
+  }
+};
+
+const userAuth = (req, res, next) => {
+  const token = "abc";
+  if (token === "abc") {
+    console.log("User Auth sucessful");
+    next();
+  } else {
+    res.send("Invalid Token");
+  }
+};
+
+module.exports = { adminAuth, userAuth };
