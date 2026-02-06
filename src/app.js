499527651a9776dcbf32ca7d198fae82d7fa67c8
Vishal Sahgal <mrvishalsahgal@gmail.com>
2026-02-06 16:42:53 +0530

    feat :  schema validation and sanitization

--- src/app.js
+++ src/app.js
@@ -16,6 +16,66 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//feed find will give in array
+app.get("/feed", async (req, res) => {
+  console.log(req.body);
+  try {
+    const user = await User.findById(req.body);
+    if (user.length === 0) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const allowedUpdates = ["age", "gender", "skills", "userId"];
+    const updates = Object.keys(req.body);
+    const isValidUpdate = updates.every((update) =>
+      allowedUpdates.includes(update),
+    );
+    if (!isValidUpdate) {
+      return res.status(400).send("Invalid update");
+    }
+
+    const user = await User.findByIdAndUpdate(userId, req.body, {
+      returnDocument: "before",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("MongoDB connected");
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,53 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 60,
+    },
+    gender: {
+      type: String,
+      validate: {
+        validator: function (value) {
+          return value === "Male" || value === "Female" || value === "Other";
+        },
+        message: "Gender must be Male, Female or Other",
+      },
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
   },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
